bd99e13f941dde6c894a021d80c3e6a350883ee4
aasritaemani <83127262+aasritaemani@users.noreply.github.com>
2021-04-25 23:24:37 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,22 @@
+# part - 1 [Take variables with values of different types]
+Name = "sara"
+Age = 20
+College = "Bhavans women college"
+Height = 5.5
+Obesity = False
+
+# part - 2 [Print these in different lines and with appropriate messages (use .format()]
+print("My name is {}.".format(Name))
+print("I am {} years old.".format(Age))
+print("I am from {}.".format(College))
+print("My height is {}inches.".format(Height))
+print("I have obesity : {}".format(Obesity))
+
+# part - 3 [Real world example of variable apart from which i gave in class]
+""" 
+Variables are numbers that don't have a fixed value, 
+examples : 
+1. Distance and time during a trip.
+2. Amount of wages earned and hours worked by a labour.
+3. Temperature changes daily based upon the climate.
+"""
